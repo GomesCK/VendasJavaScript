b48a03dea1cef4c497a1d05adddfb6f627c8f5e2
Afonso Gomes <133511515+GomesCK@users.noreply.github.com>
2024-05-09 19:13:50 -0300

    Update remove-enc.js

--- js/remove-enc.js
+++ js/remove-enc.js
@@ -9,7 +9,7 @@ encomendas.forEach(function(linha){
     });
 });*/
 
-var tabela = document.querySelector("table");
+var tabela = document.querySelector("tbody");
 
 tabela.addEventListener("dblclick", function(event){
     
